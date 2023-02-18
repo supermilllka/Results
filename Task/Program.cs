fa74817bd94c9a692aaf5c66895592093c584709
supermilllka <milka.87@bk.ru>
2023-02-18 18:33:06 +0400

    Метод печати массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,3 +5,20 @@
 ["Russia", "Denmark", "Kazan"] -> []*/
 
 string[] Array = {"hello", "2", "world", ":-)"};
+
+
+
+
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write(arr[i] + ",");
+        else
+            Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
